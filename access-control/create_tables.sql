f322c8942377268739835573c151ffd8f7952212
Hollow Man <hollowman@opensuse.org>
2023-11-21 17:15:58 +0100

    Add roles table
    Signed-off-by: Hollow Man <hollowman@opensuse.org>

--- access-control/create_tables.sql
+++ access-control/create_tables.sql
@@ -5,6 +5,21 @@ CREATE TABLE IF NOT EXISTS users (
   PRIMARY KEY (username)
 );
 
+
+CREATE TABLE IF NOT EXISTS roles (
+  role varchar(12) NOT NULL,
+  print int NOT NULL,
+  queue int NOT NULL,
+  top_queue int NOT NULL,
+  start int NOT NULL,
+  stop int NOT NULL,
+  restart int NOT NULL,
+  status int NOT NULL,
+  read_config int NOT NULL,
+  set_config int NOT NULL,
+  PRIMARY KEY (role)
+);
+
 CREATE TABLE IF NOT EXISTS access_control_list (
   username varchar(12) NOT NULL,
   print int NOT NULL,
